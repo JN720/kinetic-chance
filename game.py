c845f8e7851a77007d111d967e3ffc2dd33ad165
JN720 <112584623+JN720@users.noreply.github.com>
2023-10-06 18:50:37 -0700

    update

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import random
 PS = 20
 
 class Player():
-  def __init__(self, hp, spd, moveset, color, control, x = 0, y = 0):
+  def __init__(self, hp, spd, moveset, color, control, x = 0, y = 0, mr = 0.3):
     self.hp = hp
     self.maxhp = hp
     self.base_spd = spd
@@ -13,6 +13,10 @@ class Player():
     self.color = color
     self.x = x
     self.y = y
+    self.mx = 0
+    self.my = 0
+    self.dx = PS * 2
+    self.dy = PS * 2
     self.mana = 0
     self.lit = 0
     self.streak = 0
@@ -27,13 +31,15 @@ class Player():
     self.selcd = 0
     self.castseq = []
     self.delays = []
+    self.perm = 1
+    self.mr = mr
     #right left down up streak lit cast
     if control == 0:
       self.controls = ['d', 'a', 's', 'w', 'q', 'e', 'f', 'c', 'x'] 
     elif control == 1:
       self.controls = ['l', 'j', 'k', 'i', 'u', 'o', ';', '.', ',']
     elif control == 2:
-      self.controls = [[0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1], [0, 1, 0], [0, 0, 1], [0, 1], -1, -1]
+      self.controls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   def render(self, display):
     if self.effs[0] > 0:
       pygame.draw.circle(display, (0, 150, 0, 0), (self.x + PS, self.y + PS), PS + 5, 3)
@@ -42,9 +48,9 @@ class Player():
     if self.effs[2] > 0:
       pygame.draw.circle(display, (150, 0, 0, 0), (self.x + PS, self.y + PS), PS + 9, 3)
     if self.stun > 0:
-      pygame.draw.circle(display, (0, 150, 0, 0), (self.x + PS, self.y + PS), PS + 11, 3)
+      pygame.draw.circle(display, (0, 0, 150, 0), (self.x + PS, self.y + PS), PS + 11, 3)
     pygame.draw.rect(display, self.color, pygame.Rect(self.x, self.y, PS * 2, PS * 2))
-    pygame.draw.rect(display, (0, 255, 0), pygame.Rect(self.x - (PS * 0.5), self.y - 20, self.hp * PS * 1.5 / 100, 8))
+    pygame.draw.rect(display, (0, 255, 0), pygame.Rect(self.x - (PS * 0.5), self.y - 20, self.hp * PS * 3 / self.maxhp, 8))
     pygame.draw.rect(display, (100, 100, 255), pygame.Rect(self.x - (PS * 0.5), self.y - 12, self.mana * PS * 1.5 / 50, 8))
     if self.shield > 0:
       pygame.draw.rect(display, (100, 100, 255), pygame.Rect(self.x, self.y, PS * 2, PS * 2), self.shield // 8)
@@ -82,16 +88,16 @@ class Player():
   def control(self, key):
     #movement
     if key == self.controls[0]:
-      self.x += self.base_spd
+      self.mx = self.base_spd
       self.dir = 0
     elif key == self.controls[1]:
-      self.x -= self.base_spd
+      self.mx = -self.base_spd
       self.dir = 1
     elif key == self.controls[2]:
-      self.y += self.base_spd
+      self.my = self.base_spd
       self.dir = 2
     elif key == self.controls[3]:
-      self.y -= self.base_spd
+      self.my = -self.base_spd
       self.dir = 3
     #movement readjustment
     if self.x < 0:
@@ -138,7 +144,7 @@ class Player():
         move = base.copy()
         move.init(self.dir, self.x, self.y)
         return move
-      self.castcd = 10
+      self.castcd = 20
     #select
     elif key == self.controls[7] and self.selcd == 0:
       self.selmove += 1
@@ -175,7 +181,7 @@ class Player():
     if self.selcd > 0:
       self.selcd -= 1
     #mana and streak
-    self.mana += 0.3
+    self.mana += self.mr
     if self.mana > 100:
       self.mana = 100
     if self.scd > 0:
@@ -204,6 +210,12 @@ class Player():
     if self.hp <= 0:
       self.alive = False
       
+  def move(self):
+    self.x += self.mx
+    self.y += self.my
+    self.mx = 0
+    self.my = 0
+
 #damage, heal, poison, burn, stun, shield, regen
 class Move():
   def __init__(self, cost = 1, damage = 0, heal = 0, poison = 0, burn = 0, stun = 0, shield = 0, regen = 0, cleanse = 0):
@@ -236,15 +248,18 @@ class Move():
     return rmove
     
 class Melee(Move):
-  def __init__(self, xsize, ysize, color, lifetime, cost = 1, damage = 0, heal = 0, poison = 0, burn = 0, stun = 0, shield = 0, regen = 0, cleanse = 0):
+  def __init__(self, xsize, ysize, color, lifetime, cost = 1, damage = 0, heal = 0, poison = 0, burn = 0, stun = 0, shield = 0, regen = 0, cleanse = 0, perm = 1):
     super().__init__(cost, damage, heal, poison, burn, stun, shield, regen, cleanse)
     self.xsize = xsize
     self.ysize = ysize
     self.dx = xsize
     self.dy = ysize
+    self.mx = 0
+    self.my = 0
     self.color = color
     self.movetype = 1
     self.lifetime = lifetime
+    self.perm = perm
   def copy(self):
     rmove = Melee(0, 0, 0, 0, damage = self.damage)
     rmove.cost = self.cost
@@ -260,6 +275,7 @@ class Melee(Move):
     rmove.ysize = self.ysize
     rmove.color = self.color
     rmove.lifetime = self.lifetime
+    rmove.perm = self.perm
     return rmove
   def render(self, display):
       pygame.draw.rect(display, self.color, pygame.Rect(self.x, self.y, self.dx, self.dy))
@@ -270,32 +286,31 @@ class Melee(Move):
     return False
   def init(self, dir, x, y):
     self.dir = dir
-    match self.dir:
-      case 0:
-        self.dx = self.xsize
-        self.dy = self.ysize
-        self.x = x + 15
-        self.y = y + 15 - (self.dy / 2)
-      case 1:
-        self.dx = self.xsize
-        self.dy = self.ysize
-        self.x = x + 15 - self.dx
-        self.y = y + 15 - (self.dy / 2)
-      case 2:
-        self.dx = self.ysize
-        self.dy = self.xsize
-        self.y = y + 15
-        self.x = x + 15 - (self.dx / 2)
-      case 3:
-        self.dx = self.ysize
-        self.dy = self.xsize
-        self.y = y + 15 - self.dy
-        self.x = x + 15 - (self.dx / 2)
+    if self.dir == 0:
+      self.dx = self.xsize
+      self.dy = self.ysize
+      self.x = x + 15
+      self.y = y + 15 - (self.dy / 2)
+    elif self.dir == 1:
+      self.dx = self.xsize
+      self.dy = self.ysize
+      self.x = x + 15 - self.dx
+      self.y = y + 15 - (self.dy / 2)
+    elif self.dir == 2:
+      self.dx = self.ysize
+      self.dy = self.xsize
+      self.y = y + 15
+      self.x = x + 15 - (self.dx / 2)
+    elif self.dir == 3:
+      self.dx = self.ysize
+      self.dy = self.xsize
+      self.y = y + 15 - self.dy
+      self.x = x + 15 - (self.dx / 2)
 
     
 class Projectile(Melee):
-  def __init__(self, speed, xsize, ysize, color, lifetime, cost = 1, damage = 0, heal = 0, poison = 0, burn = 0, stun = 0, shield = 0, regen = 0, cleanse = 0, spread = 0):
-    super().__init__(xsize, ysize, color, lifetime, cost, damage, heal, poison, burn, stun, shield, regen, cleanse)
+  def __init__(self, speed, xsize, ysize, color, lifetime, cost = 1, damage = 0, heal = 0, poison = 0, burn = 0, stun = 0, shield = 0, regen = 0, cleanse = 0, spread = 0, perm = 1):
+    super().__init__(xsize, ysize, color, lifetime, cost, damage, heal, poison, burn, stun, shield, regen, cleanse, perm)
     self.spread = spread
     self.speed = speed
     self.alive = True
@@ -317,21 +332,21 @@ class Projectile(Melee):
     rmove.ysize = self.ysize
     rmove.color = self.color
     rmove.lifetime = self.lifetime
+    rmove.perm = self.perm
     return rmove
   def tick(self):
-    match self.dir:
-      case 0:
-        self.x += self.speed
-        self.y += self.spread
-      case 1:
-        self.x -= self.speed
-        self.y += self.spread
-      case 2:
-        self.y += self.speed
-        self.x += self.spread
-      case 3:
-        self.y -= self.speed
-        self.x += self.spread
+    if self.dir == 0:
+      self.x += self.speed
+      self.y += self.spread
+    elif self.dir == 1:
+      self.x -= self.speed
+      self.y += self.spread
+    elif self.dir == 2:
+      self.y += self.speed
+      self.x += self.spread
+    elif self.dir == 3:
+      self.y -= self.speed
+      self.x += self.spread
     return super().tick()
   def init(self, dir, x, y):
     super().init(dir, x, y)
@@ -349,24 +364,24 @@ class MultiSpecial():
   def copy(self):
     seq = []
     for i in range(self.repeat):
-      match self.specid:
-        case 0:
-          seq.append(self.move.copy())
-        case 1: #arrow
-          r = random.randint(0, 4)
-          amove = Projectile(3, 20, 6, (100, 50, 0), 120)
-          match r:
-            case 1:
-              amove.damage = 10
-              amove.speed = 6
-            case 2:
-              amove.damage = 30
-              amove.speed = 9
-            case 3:
-              amove.damage = 50
-              amove.stun = 60
-              amove.speed = 12
-          seq.append(amove)
+      if self.specid == 0:
+        seq.append(self.move.copy())
+      elif self.specid == 1: #arrow
+        r = random.randint(0, 4)
+        amove = Projectile(3, 20, 6, (100, 50, 0), 120)
+        if r == 1:
+          amove.damage = 10
+          amove.speed = 6
+        elif r == 2:
+          amove.damage = 30
+          amove.speed = 9
+          amove.perm = 2
+        elif r == 3:
+          amove.damage = 50
+          amove.stun = 60
+          amove.speed = 12
+          amove.perm = 2
+        seq.append(amove)
     return seq, self.delays
 
 #trigger: cost, attributes
@@ -380,29 +395,29 @@ thrown_sword = Projectile(6, 40, 6, (200, 200, 200), 60, 1, damage = 10, spread 
 MOVES = {
   #1 streak
   'sword' : Melee(40, 6, (200, 200, 200), 60, 1, damage = 10),
-  'flare' : Projectile(5, 15, 15, (255, 100, 0), 180, 1, burn = 1),
+  'flare' : Projectile(5, 15, 15, (255, 100, 0), 180, 1, burn = 1, perm = 0),
   #2 streak
   'heal' : Move(2, heal = 20, cleanse = 1),
   'regenerate' : Move(2, regen = 4),
   'shield' : Move(2, shield = 40),
   'intoxicate' : Projectile(4, 15, 6, (0, 150, 0), 60, 2, damage = 10, poison = 3),
-  'zap' : Projectile(15, 5, 50, (0, 0, 255), 10, 2, damage = 20, stun = 60),
+  'zap' : Projectile(15, 5, 50, (0, 0, 255), 10, 2, damage = 20, stun = 60, perm = 3),
   'arrow' : MultiSpecial(cost = 2, specid = 1),
   'multisword' : MultiSpecial(acc_thrown_sword.copy(), 2, 3, [0, 10, 10]),
   #3 streak
-  'cannon' : Projectile(8, 25, 25, (0, 0, 0), 300, 3, damage = 50),
-  'fireball' : Projectile(6, 30, 20, (255, 100, 0), 180, 3, damage = 30, burn = 4),
+  'cannon' : Projectile(8, 25, 25, (0, 0, 0), 300, 3, damage = 50, perm = 2),
+  'fireball' : Projectile(6, 30, 20, (255, 100, 0), 180, 3, damage = 30, burn = 4, perm = 2),
   'asian slap' : Melee(40, 40, (255, 255, 0), 20, 3, damage = 30, stun = 120),
   'blade flurry' : MultiSpecial(thrown_sword.copy(), 3, 8, [0, 8, 8, 8, 8, 8, 8, 8]),
-  'micronuke' : Projectile(5, 25, 20, (200, 200, 0), 300, 3, damage = 30, poison = 3, burn = 2),
+  'micronuke' : Projectile(5, 25, 20, (200, 200, 0), 300, 3, damage = 30, poison = 3, burn = 2, perm = 2),
   #4 streak
   'mega shield' : Move(4, shield = 100),
   'mega heal' : Move(4, heal = 100, cleanse = 4),
-  'superstrike' : Projectile(12, 8, 70, (230, 230, 230), 180, 4, damage = 85),
+  'superstrike' : Projectile(12, 8, 70, (230, 230, 230), 180, 4, damage = 85, perm = 3),
   'volley' : MultiSpecial(cost = 4, repeat = 4, delays = [0, 8, 8, 8], specid = 1),
   'blade storm' : MultiSpecial(thrown_sword.copy(), 4, 12, [0, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]),
   'asphyxiate' : Projectile(3, 20, 8, (0, 120, 0), 240, 4, poison = 14),
-  'milinuke' : Projectile(4, 30, 24, (180, 180, 0), 300, 4, damage = 50, poison = 5, burn = 3, stun = 30),
+  'milinuke' : Projectile(4, 30, 24, (180, 180, 0), 300, 4, damage = 50, poison = 5, burn = 3, stun = 30, perm = 2),
   #5 streak
   'parry this' : Projectile(14, 10, 10, (30, 30, 30), 300, 5, damage = 140)
 }
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 import pygame.locals
 import game
+from random import randint
 
 pygame.init()
 DISPLAY = pygame.display.set_mode((800, 450), flags = pygame.SCALED)
@@ -9,41 +10,173 @@ pygame.display.set_caption('Kinetic Chance')
 players = []
 spells = []
 
+colors = [
+  (0, 0, 0), #black
+  (120, 40, 20), #dark brown
+  (180, 180, 180), #grey
+  (100, 100, 155),  #light blue
+  (155, 100, 100), #light red
+  (255, 100, 0) #elusive orange
+  ]
+
+map_names = ['The Void', 'Classic', 'Divided']
+backgrounds = [(40, 40, 40), (30, 30, 0), (110, 110, 110)]
+#one list per map
+#each list contains lists of length 6 with terrain attributes
+#x, y, xsize, ysize, color id, antipermeability
+terrains = [
+  [],
+  [
+    [0, 0, 100, 450, 3, 0],
+    [700, 0, 100, 450, 4, 0],
+    [300, 0, 200, 100, 0, 3],
+    [300, 350, 200, 100, 0, 3],
+    [180, 100, 50, 250, 1, 2],
+    [570, 100, 50, 250, 1, 2],
+  ],
+  [
+    [0, 0, 800, 40, 0, 3],
+    [0, 410, 800, 40, 0, 3],
+    [0, 40, 100, 100, 0, 3],
+    [0, 310, 100, 100, 0, 3],
+    [700, 40, 100, 100, 0, 3],
+    [700, 310, 100, 100, 0, 3],
+    [200, 125, 400, 15, 2, 2],
+    [200, 310, 400, 15, 2, 2],
+    [100, 125, 100, 15, 5, 1],
+    [100, 310, 100, 15, 5, 1],
+    [600, 125, 100, 15, 5, 1],
+    [600, 310, 100, 15, 5, 1]
+  ]
+]
+map = 0
+
+mode = 0
+mode_names = ['Classic', 'Freeplay', 'Random', 'Draft']
+mode_descriptions = [
+  'A simple mode with 4 moves and standard stats',
+  'A high-stat mode where every move is accessible',
+  'A mode with randomized moves',
+  'A competitive mode where moves are drafted'
+]
+
 pause = False
 running = True
 game_running = False
+drafting = False
+winner = -1
 
 def collision(player, spell):
   if ((spell.x > player.x and spell.x < player.x + game.PS * 2) or (spell.x + spell.dx > player.x and spell.x + spell.dx < player.x + game.PS * 2)) and ((spell.y > player.y and spell.y < player.y + game.PS * 2) or (spell.y + spell.dy > player.y and spell.y + spell.dy < player.y + game.PS * 2)):
     return True
-  if ((player.x > spell.x and player.x < spell.x + spell.dx) or (player.x + player.x + game.PS * 2 > spell.x and player.x + player.x + game.PS * 2 < spell.x + spell.dx)) and ((player.y > spell.y and player.y < spell.y + spell.dy) or (player.y + game.PS * 2 > spell.y and player.y + game.PS * 2 < spell.y + spell.dy)):
+  if ((player.x > spell.x and player.x < spell.x + spell.dx) or (player.x + game.PS * 2 > spell.x and player.x + game.PS * 2 < spell.x + spell.dx)) and ((player.y > spell.y and player.y < spell.y + spell.dy) or (player.y + game.PS * 2 > spell.y and player.y + game.PS * 2 < spell.y + spell.dy)):
     return True
+  return False
+
+def render_terrain():
+  for rect in terrains[map]:
+    pygame.draw.rect(DISPLAY, colors[rect[4]], pygame.Rect(*rect[0:4]))
+
+def terrain_collision(r):
+  x = r.x + r.mx
+  y = r.y + r.my
+  for t in terrains[map]:
+    #check if projectile/player can phase through
+    if r.perm >= t[5]:
+      continue
+    #rectangle collision
+    if ((x > t[0] and x < t[0] + t[2]) or (x + r.dx > t[0] and x + r.dx < t[0] + t[2])) and ((y > t[1] and y < t[1] + t[3]) or (y + r.dy > t[1] and y + r.dy < t[1] + t[3])):
+      return True
+    if ((t[0] > x and t[0] < x + r.dx) or (t[0] + t[2] > x and t[0] + t[2] < x + r.dx)) and ((t[1] > r.y and t[1] < y + r.dy) or (t[1] + t[3] > y and t[1] + t[3] < y + r.dy)):
+      return True
+  return False
+
+def terrain_collision_x(r):
+  x = r.x + r.mx
+  y = r.y
+  for t in terrains[map]:
+    #check if projectile/player can phase through
+    if r.perm >= t[5]:
+      continue
+    #rectangle collision
+    if ((x > t[0] and x < t[0] + t[2]) or (x + r.dx > t[0] and x + r.dx < t[0] + t[2])) and ((y > t[1] and y < t[1] + t[3]) or (y + r.dy > t[1] and y + r.dy < t[1] + t[3])):
+      return True
+    if ((t[0] > x and t[0] < x + r.dx) or (t[0] + t[2] > x and t[0] + t[2] < x + r.dx)) and ((t[1] > r.y and t[1] < y + r.dy) or (t[1] + t[3] > y and t[1] + t[3] < y + r.dy)):
+      return True
+  return False
+
+def terrain_collision_y(r):
+  x = r.x
+  y = r.y + r.my
+  for t in terrains[map]:
+    #check if projectile/player can phase through
+    if r.perm >= t[5]:
+      continue
+    #rectangle collision
+    if ((x > t[0] and x < t[0] + t[2]) or (x + r.dx > t[0] and x + r.dx < t[0] + t[2])) and ((y > t[1] and y < t[1] + t[3]) or (y + r.dy > t[1] and y + r.dy < t[1] + t[3])):
+      return True
+    if ((t[0] > x and t[0] < x + r.dx) or (t[0] + t[2] > x and t[0] + t[2] < x + r.dx)) and ((t[1] > r.y and t[1] < y + r.dy) or (t[1] + t[3] > y and t[1] + t[3] < y + r.dy)):
+      return True
   return False
 
 font = pygame.font.SysFont('dejavusans', 30)
 sfont = pygame.font.SysFont('dejavusans', 20)
 
 p1moves = list(game.MOVES.keys())
-
 p2moves = list(game.MOVES.keys())
 
 while running:
-  DISPLAY.fill((40, 40, 40))
-
+  DISPLAY.fill(backgrounds[map])
+  render_terrain()
   for event in pygame.event.get():
     if event.type == pygame.locals.QUIT:
       running = False
       pygame.quit()
       sys.exit()
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_s and not game_running:
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_g and not game_running:
+      #classic
+      if mode == 0:
+        p1moves = ['sword', 'heal', 'fireball', 'superstrike']
+        p2moves = ['sword', 'heal', 'fireball', 'superstrike']
+        players = [
+          game.Player(200, 3, p1moves, (0, 0, 255), 0, 80, 205),
+          game.Player(200, 3, p2moves, (255, 0, 0), 1, 720, 205)]
+      #frenzy
+      elif mode == 1:
+        p1moves = list(game.MOVES.keys())
+        p2moves = list(game.MOVES.keys())
+        players = [
+          game.Player(500, 4, p1moves, (0, 0, 255), 0, 80, 205, mr = 0.6),
+          game.Player(500, 4, p2moves, (255, 0, 0), 1, 720, 205, mr = 0.6)]
+      #random
+      elif mode == 2:
+        moves = list(game.MOVES.keys())
+        p1moves = []
+        p2moves = []
+        for i in range(6):
+          p1moves.append(moves[randint(0, len(moves) - 1)])
+          p2moves.append(moves[randint(0, len(moves) - 1)])
+          players = [
+            game.Player(300, 3, p1moves, (0, 0, 255), 0, 80, 205),
+            game.Player(300, 3, p2moves, (255, 0, 0), 1, 720, 205)
+          ]
       game_running = True
-      players = [game.Player(200, 4, p1moves, (0, 0, 255), 0, 80, 280),
-          game.Player(200, 4, p2moves, (255, 0, 0), 1, 720, 280)]
       spells = []
+      
+        
     if pause and event.type == pygame.KEYDOWN and event.key == pygame.K_p and game_running:
       pause = False
     elif not pause and event.type == pygame.KEYDOWN and event.key == pygame.K_p and game_running:
       pause = True
+    #arrow keys for map selection
+    if not game_running and event.type == pygame.KEYDOWN and event.key == pygame.K_a and map > 0:
+      map -= 1
+    elif not game_running and event.type == pygame.KEYDOWN and event.key == pygame.K_d and map < len(map_names) - 1:
+      map += 1
+    if not game_running and event.type == pygame.KEYDOWN and event.key == pygame.K_w and mode > 0:
+      mode -= 1
+    elif not game_running and event.type == pygame.KEYDOWN and event.key == pygame.K_s and mode < len(mode_names) - 1:
+      mode += 1
       
   if game_running:
     #text display
@@ -55,6 +188,7 @@ while running:
     DISPLAY.blit(sfont.render(players[1].moveset[-1] if players[1].selmove == 0 else players[1].moveset[players[1].selmove - 1], True, (150, 150, 150)), (500, 80))
 
     if pause:
+      #display unpause and render players and spells during pause
       DISPLAY.blit(font.render('Press P to Unpause', True, (255, 255, 255)), (350, 150))
       for i in range(len(players)):
         if not players[i].alive:
@@ -64,44 +198,80 @@ while running:
       for j in range(len(spells)):
         spells[j].render(DISPLAY)
     else:
+      #counting alive players
       alive = 0
       for i in range(len(players)):
         if not players[i].alive:
           continue
         alive += 1
+        #if only 1 player is alive, declare the winner
+        winner = i
+        #handle player logic and casting
         players[i].tick()
         newspell = players[i].autocast()
         if newspell != None:
           spells.append(newspell)
           spells[-1].owner = i
+        #rendering and controls
         players[i].render(DISPLAY)
         keys = pygame.key.get_pressed()
         for k in range(len(keys)):
+          #disable if stunned
           if keys[k] and pygame.key.name(k) in players[i].controls and players[i].stun == 0:
             newspell = players[i].control(pygame.key.name(k))
             if newspell != None:
               spells.append(newspell)
               spells[-1].owner = i
+        #apply movement
+        if terrain_collision_x(players[i]) and not terrain_collision_y(players[i]):
+          players[i].mx = 0
+          players[i].move()
+        elif terrain_collision_y(players[i]) and not terrain_collision_x(players[i]):
+          players[i].my = 0
+          players[i].move()
+        if terrain_collision(players[i]):
+          players[i].mx = 0
+          players[i].my = 0
+        else:
+          players[i].move()
+        #handle spell logic
         for j in range(len(spells)):
             if spells[j] == -1:
                 continue
             if i != spells[j].owner:
+                #spell interaction on players
                 if collision(players[i], spells[j]):
                     players[i].trigger(spells[j])
                     spells[j] = -1
             elif spells[j].movetype == 1:
                 spells[j].init(players[i].dir, players[i].x, players[i].y)
+      #mark ended spells to be deleted and remove them
       for i in range(len(spells)):
         if spells[i] == -1:
           continue
         spells[i].render(DISPLAY)
-        if spells[i].tick():
+        if spells[i].tick() or terrain_collision(spells[i]):
           spells[i] = -1
       while -1 in spells:
         spells.remove(-1)
+      #win condition
       if alive == 1:
         game_running = False
+  elif drafting:
+    pass
   else:
-    DISPLAY.blit(font.render('Press S to Start', True, (255, 255, 255)), (300, 200))
+    DISPLAY.blit(font.render('Press G to Start', True, (255, 255, 255)), (300, 200))
+    DISPLAY.blit(font.render('Use A and D to Change Map', True, (150, 150, 150)), (220, 100))
+    DISPLAY.blit(font.render('Use W and S to Change Mode', True, (150, 150, 150)), (210, 150))
+    #display last game's winner
+    if winner > -1:
+      DISPLAY.blit(font.render('Player ' + str(winner + 1) + ' Wins!', True, (255, 255, 0)), (300, 300))
+    #display map names
+    for i in range(len(map_names)):
+      DISPLAY.blit(font.render(map_names[i], True, (255, 255, 255) if map == i else (150, 150, 150)), ((i * 150) + 10, 20))
+    for i in range(len(mode_names)):
+      DISPLAY.blit(font.render(mode_names[i], True, (255, 255, 255) if mode == i else (150, 150, 150)), (((i * 150) + 10, 400)))
+    DISPLAY.blit(sfont.render(mode_descriptions[mode], True, (255, 255, 255)), (10, 350))
+  #render map (this applies regardless of if the game runs)
   pygame.display.update()
   pygame.time.Clock().tick(60)
